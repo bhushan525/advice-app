af675880559ba1e0f7b9ebd1684dea1d6263bb5a
bhushan <dahalebhushan525@gmail.com>
2023-12-22 15:22:54 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useEffect, useState, useCallback } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [advice, setAdvice] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  // Function to fetch advice
+  const fetchAdvice = useCallback(async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get('https://api.adviceslip.com/advice');
+      setAdvice(response.data.slip.advice);
+    } catch (error) {
+      console.error('Error:', error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    // Fetch advice when the component mounts
+    fetchAdvice();
+  }, [fetchAdvice]);
+
+  // Function to fetch new advice when the button is clicked
+  const handleClick = () => {
+    // Fetch advice again
+    fetchAdvice();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="card">
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            <h1 className="heading">{advice}</h1>
+            <button className='button' onClick={handleClick}>
+              <span>Click here to get Advice</span>
+            </button>
+          </>
+        )}
+      </div>
     </div>
   );
 }
